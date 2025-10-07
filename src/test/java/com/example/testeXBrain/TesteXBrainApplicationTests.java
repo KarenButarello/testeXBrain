b288fb4483afebfed510d92557e1b7f6a100a153
Karen <karenkbutarello@gmail.com>
2025-10-06 22:28:01 -0300

    Criar scripts sql de criação de tabelas e inserção de dados

---
+++ src/test/java/com/example/testeXBrain/TesteXBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testeXBrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteXBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
