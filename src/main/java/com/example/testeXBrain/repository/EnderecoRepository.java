7b8d591d49d2f351a9f172a58f58df56436133f7
Karen <karenkbutarello@gmail.com>
2025-10-08 20:06:03 -0300

    Corrige o método de criar nova venda

--- src/main/java/com/example/testeXBrain/repository/EnderecoRepository.java
+++ src/main/java/com/example/testeXBrain/repository/EnderecoRepository.java
@@ -4,8 +4,6 @@ import com.example.testeXBrain.model.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface EnderecoRepository extends JpaRepository <Endereco, Integer> {
 
